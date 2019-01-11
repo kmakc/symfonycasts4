607f6a757f41a17b4833414c185842da7845dc63
Maxim Kovalsky <kmakc@yandex.ru>
2019-01-11 16:24:12 +0300

    fundamentals: creating service

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Service\MarkdownHelper;
 use Michelf\MarkdownInterface;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\Cache\Adapter\AdapterInterface;
@@ -24,7 +25,7 @@ class ArticleController extends AbstractController
     /**
      * @Route("/news/{slug}", name="article_show")
      */
-    public function show($slug, MarkdownInterface $markdown, AdapterInterface $cache)
+    public function show($slug, MarkdownHelper $markdownHelper)
     {
         $comments = [
             'First comment',
@@ -53,13 +54,7 @@ exercitation eiusmod. Exercitation incididunt rump laborum, t-bone short ribs bu
 bresaola shoulder burgdoggen fugiat. Adipisicing nostrud chicken consequat beef ribs, quis filet mignon do.
 Prosciutto capicola mollit shankle aliquip do dolore hamburger brisket turducken eu.
 EOF;
-
-        $item = $cache->getItem('markdown_' . md5($articleContent));
-        if (!$item->isHit()) {
-            $item->set($markdown->transform($articleContent));
-            $cache->save($item);
-        }
-        $articleContent = $item->get();
+        $articleContent = $markdownHelper->parse($articleContent);
 
         return $this->render('article/show.html.twig', [
             'title'          => ucwords(str_replace('-', ' ', $slug)),
