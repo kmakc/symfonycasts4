38666304a553213d10904c2e625ffedfc51b64a6
Maxim Kovalsky <kmakc@yandex.ru>
2019-01-15 16:02:33 +0300

    database: own custom twig extension (filter)

---
+++ src/Twig/AppExtension.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Twig;
+
+use App\Service\MarkdownHelper;
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+
+
+class AppExtension extends AbstractExtension
+{
+    /**
+     * @var MarkdownHelper
+     */
+    private $helper;
+
+    public function __construct(MarkdownHelper $helper)
+    {
+        $this->helper = $helper;
+    }
+
+    public function getFilters(): array
+    {
+        return [
+            new TwigFilter('cached_markdown', [$this, 'processMarkdown'], ['is_safe' => ['html']]),
+        ];
+    }
+
+    public function processMarkdown($value)
+    {
+        return $this->helper->parse($value);
+    }
+}
