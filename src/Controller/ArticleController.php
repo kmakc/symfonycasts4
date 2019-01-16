d85b79f6807a5d93397c2ab40d8cb5be4ccd5633
Maxim Kovalsky <kmakc@yandex.ru>
2019-01-16 18:41:07 +0300

    database: updating an entity

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -58,15 +58,16 @@ class ArticleController extends AbstractController
     /**
      * @Route("/news/{slug}/heart", name="article_toggle_heart", methods={"POST"})
      */
-    public function toggleArticleHeart($slug, LoggerInterface $logger)
+    public function toggleArticleHeart(Article $article, LoggerInterface $logger, EntityManagerInterface $em)
     {
-        // TODO - actually heart/unheart the article
+        $article->incrementHeartCount();
+        $em->flush();
 
         $logger->info('Article is being hearted');
         $this->logInfo('Log frm trait', [
             'message' => 'test'
         ]);
 
-        return new JsonResponse(['hearts' => rand(5, 100)]); // or return $this->json(['hearts' => rand(5, 100)]);
+        return new JsonResponse(['hearts' => $article->getHeartCount()]); // or return $this->json(['hearts' => rand(5, 100)]);
     }
 }
