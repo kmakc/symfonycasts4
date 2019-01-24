f6992f0bf58bbd62ae4a8c4094911d8fd91ab884
Maxim Kovalsky <kmakc@yandex.ru>
2019-01-24 13:59:27 +0300

    security: dynamic roles

--- src/DataFixtures/UserFixture.php
+++ src/DataFixtures/UserFixture.php
@@ -25,7 +25,18 @@ class UserFixture extends BaseFixture
             $user->setEmail(sprintf('spacebar%d@example.com', $i));
             $user->setFirstName($this->faker->firstName);
 
-            $user->setPassword($this->passwordEncoder->encodePassword($user, 'p@$$woRd'));
+            $user->setPassword($this->passwordEncoder->encodePassword($user, 'pswd'));
+
+            return $user;
+        });
+
+        $this->createMany(3, 'admin_users', function ($i) {
+            $user = new User();
+            $user->setEmail(sprintf('admin%d@example.com', $i));
+            $user->setFirstName($this->faker->firstName);
+            $user->setRoles(['ROLE_ADMIN']);
+
+            $user->setPassword($this->passwordEncoder->encodePassword($user, 'pswd'));
 
             return $user;
         });
