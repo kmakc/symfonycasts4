6d976f66e2b62fa1f94e3de09e5fd024b69feb89
Maxim Kovalsky <kmakc@yandex.ru>
2019-01-28 12:06:30 +0300

    forms: success (flash) message

--- src/Controller/ArticleAdminController.php
+++ src/Controller/ArticleAdminController.php
@@ -36,7 +36,9 @@ class ArticleAdminController extends AbstractController
             $em->persist($article);
             $em->flush();
 
-            return $this->redirectToRoute('app_homepage');
+            $this->addFlash('success', 'Article created successful!');
+
+            return $this->redirectToRoute('admin_article_list');
         }
 
         return $this->render('article_admin/new.html.twig', [
@@ -60,7 +62,7 @@ class ArticleAdminController extends AbstractController
     }
 
     /**
-     *  @Route("/admin/article")
+     *  @Route("/admin/article", name="admin_article_list")
      */
     public function list(ArticleRepository $articleRepository)
     {
