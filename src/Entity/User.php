498130b596c5ab2f66d574eabbe393b5f707f2c0
Maxim Kovalsky <kmakc@yandex.ru>
2019-01-29 16:21:16 +0300

    forms: agree to terms database field

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -66,6 +66,11 @@ class User implements UserInterface
      */
     private $articles;
 
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $agreedTermsAt;
+
     public function __construct()
     {
         $this->apiTokens = new ArrayCollection();
@@ -251,5 +256,17 @@ class User implements UserInterface
     public function __toString() // for correct render in article.homepage.twig
     {
         return $this->getFirstName();
+    }
+
+    public function getAgreedTermsAt(): ?\DateTimeInterface
+    {
+        return $this->agreedTermsAt;
+    }
+
+    public function setAgreedTermsAt(\DateTimeInterface $agreedTermsAt): self
+    {
+        $this->agreedTermsAt = $agreedTermsAt;
+
+        return $this;
     }
 }
--- src/Form/UserRegistrationFormType.php
+++ src/Form/UserRegistrationFormType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -31,7 +32,8 @@ class UserRegistrationFormType extends AbstractType
                         'maxMessage'=> 'too long',
                     ])
                 ]
-            ]);
+            ])
+            ->add('agreeTerms', CheckboxType::class);
     }
 
     public function configureOptions(OptionsResolver $resolver)
