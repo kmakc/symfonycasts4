e9aa896cb80405390678e1b6a66490abb78be190
Maxim Kovalsky <kmakc@yandex.ru>
2019-01-31 18:22:05 +0300

    forms: a bit of security cleanup

--- src/Controller/BaseController.php
+++ src/Controller/BaseController.php
@@ -7,8 +7,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 abstract class BaseController extends AbstractController
 {
+    // : User для уточнения того что возвращается именно User App\Entity\User
     public function getUser(): User
     {
         return parent::getUser();
+        // or in annotation:
+        /**
+         * @method User getUser()
+         */
     }
 }
